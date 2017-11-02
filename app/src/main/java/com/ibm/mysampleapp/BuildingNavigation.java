6fc1d882f1d493c6cb60870a37e144dcf0953c8c
mmaricWIN <martin.maric@studenti.umb.sk>
2017-11-02 18:31:42 +0100

    UPDATE - Funkcionalita prepínania fotiek cez šípky(základ)

--- app/src/main/java/com/ibm/mysampleapp/BuildingNavigation.java
+++ app/src/main/java/com/ibm/mysampleapp/BuildingNavigation.java
@@ -1,7 +1,11 @@
 package com.ibm.mysampleapp;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by mato on 1. 11. 2017.
@@ -17,6 +21,27 @@ public class BuildingNavigation extends AppCompatActivity {
 
         //TODO UVODNA FOTKA + ZOZNAM MIESTNOSTÍ(ŠÍPKY???)
 
+        final ImageView start_image = (ImageView) findViewById(R.id.imageView);
+        final Button rightArrow = (Button) findViewById(R.id.buttonRight);
+        final Button leftArrow = (Button) findViewById(R.id.buttonLeft);
+
+        start_image.setImageResource(R.drawable.obrazok1);
+
+        rightArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_image.setImageResource(R.drawable.obrazok2);
+                rightArrow.setVisibility(View.GONE);
+            }
+        });
+
+        leftArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_image.setImageResource(R.drawable.obrazok3);
+                leftArrow.setVisibility(View.GONE);
+            }
+        });
 
 
 
